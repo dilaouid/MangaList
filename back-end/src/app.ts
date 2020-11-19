482d594771518aae6a03ee1ab1e5a9b642745c18
Diyaeddine LAOUID <yedine@live.fr>
2020-11-19 17:54:33 +0100

    commit initial front

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -3,10 +3,10 @@ import express = require('express');
 const app:  express.Application = express();
 const PORT: number = 9000;
 
-app.get('/:id', function (req, res) {
+app.get('/:id', (req, res):void => {
     res.send('Fetching ID in MaL');
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, ():void => {
     console.log(`App is listening on port ${PORT}!`);
 });
